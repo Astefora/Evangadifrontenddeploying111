17e77e57b540bbf657d3b323afdef25c0f32330a
aste-0973 <152900534+Astefora@users.noreply.github.com>
2024-03-16 03:41:05 -0400

    first commit

--- src/axiosccon.js
+++ src/axiosccon.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const axiosBase = axios.create({
-  baseURL: "http://localhost:4400/api",
+  // baseURL: "http://localhost:4400/api",
+  baseURL: "https://asterevangadi.cyclic.app/api",
+
   // baseURL: "https://nice-pear-hedgehog-fez.cyclic.app",
   // baseURL: "https://calm-hare-galoshes.cyclic.app",
 });
