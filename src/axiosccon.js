1d74eec3142ac4c96f0e77e850354a825e4acd9f
aste-0973 <152900534+Astefora@users.noreply.github.com>
2024-03-16 07:28:47 -0400

    frontendlast

--- src/axiosccon.js
+++ src/axiosccon.js
@@ -2,10 +2,11 @@ import axios from "axios";
 
 const axiosBase = axios.create({
   // baseURL: "http://localhost:4400/api",
-  baseURL: "https://asterevangadi.cyclic.app/api",
+  // baseURL: "https://asterevangadi.cyclic.app/api",
 
   // baseURL: "https://nice-pear-hedgehog-fez.cyclic.app",
   // baseURL: "https://calm-hare-galoshes.cyclic.app",
+  baseURL: "https://evangadibackendlast2024-7.onrender.com",
 });
 
 export default axiosBase;
